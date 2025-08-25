44aba198b6274966f8ed456060d850cea781c487
tumonulo <tumonulodev@gmail.com>
2025-08-25 11:20:53 +0200

    rename to matchPreview.png and create .env.example

--- src/configs/configs.js
+++ src/configs/configs.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 
 module.exports = {
     TOKEN: process.env.TOKEN,
+    PREFIX: process.env.PREFIX,
     MONGODB_URL: process.env.MONGODBURL,
     BRAWL_STARS_API_KEY: process.env.BRAWL_STARS_API_KEY,
     IMGBB_API_KEY: process.env.IMGBB_API_KEY
