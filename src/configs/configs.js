3d8c9f9e2352cc1ddebfe09d9759ff4dc397403b
Zer0Dev-exe <zeropersonalmail@gmail.com>
2025-08-25 09:48:51 +0000

    update

--- src/configs/configs.js
+++ src/configs/configs.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 module.exports = {
     TOKEN: process.env.TOKEN,
     PREFIX: process.env.PREFIX,
-    MONGODB_URL: process.env.MONGODBURL,
+    MONGODB_URL: process.env.MONGODB_URL,
     BRAWL_STARS_API_KEY: process.env.BRAWL_STARS_API_KEY,
     IMGBB_API_KEY: process.env.IMGBB_API_KEY
 }
